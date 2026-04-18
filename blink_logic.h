5d0e2367c8531ccdf4a07f22bad8a0bebcb23f3f
vash-sa <78752348+vash-sa@users.noreply.github.com>
2026-04-18 11:37:11 +0300

    Update blink_logic.h

--- blink_logic.h
+++ blink_logic.h
@@ -1,13 +1,11 @@
-#include "esphome.h"
+#include "Arduino.h"
 
-// Простая функция на C++, которую мы вызовем из YAML
-void native_blink(int times, int delay_ms) {
-    auto my_led = id(led_internal).get_output(); // Получаем доступ к компоненту по ID
+void my_blink() {
+    int pin = 8; // Пин светодиода
+    pinMode(pin, OUTPUT);
     
-    for(int i = 0; i < times; i++) {
-        my_led->set_level(1.0); // Включить (для light это float 0.0-1.0)
-        delay(delay_ms);
-        my_led->set_level(0.0); // Выключить
-        delay(delay_ms);
-    }
+    digitalWrite(pin, HIGH);
+    delay(500);
+    digitalWrite(pin, LOW);
+    delay(500);
 }
